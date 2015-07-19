6b37be277d23fa703cf59a59820a729cf5762985
tlsteinmetz <travis.steinmetz@pointgenesis.com>
2015-07-19 01:14:36 -0400

    Initial Checkin of project

---
+++ src/main/java/com/pointgenesis/main/AppMain.java
@@ -0,0 +1,19 @@
+package com.pointgenesis.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class AppMain {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
